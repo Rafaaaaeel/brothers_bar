6da4c396c9bfbf9b9c7dfa82cfeddba5efcddf70
Rafael <you@example.com>
2021-12-15 20:34:28 -0300

    #5 Open and close the card

--- src/resor/main.js
+++ src/resor/main.js
@@ -1,11 +1,23 @@
 window.onload = function(){
 
     const containerClickZone = document.querySelectorAll(".container")
+    const table = document.querySelectorAll("table")
+    const td = document.querySelectorAll("td")
 
+    td.forEach((e)=>{
+        e.style.display = "block"
+    })
+
+    table.forEach((e)=>{
+        e.style.marginLeft = "2%"
+        e.style.display = "none"
+    })
 
     containerClickZone.forEach((e)=>{
+        
         e.addEventListener("click", ()=>{
-            e.querySelector("table").style.display = e.querySelector("table").style.display === "none" ? 'initial' : 'none'
+            console.log("clicou")
+            e.querySelector("table").style.display = e.querySelector("table").style.display === "none" ? 'table' : 'none'
         })
     })
 }
