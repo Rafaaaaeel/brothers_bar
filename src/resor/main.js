aa8bd725ddfaba86c92b3cd569a03b05b6fc9244
Rafael <you@example.com>
2021-12-17 03:37:31 -0300

    #10 arrow close js working

--- src/resor/main.js
+++ src/resor/main.js
@@ -2,18 +2,11 @@ window.onload = function()
 {
     const containerClickZone = document.querySelectorAll(".container")
     const table = document.querySelectorAll("table")
-    const td = document.querySelectorAll("td")
-    const closeArrowAction = document.getElementsByClassName(".close_button")
-
-    td.forEach((e)=>
-    {
-        e.style.display = "block"
-    })
+    const closeArrowAction = document.querySelectorAll(".close_button")
 
     table.forEach((e)=>
     {
-        e.style.marginLeft = "2%"
-        e.style.display = "none" 
+        e.style.display = "none"
     })
 
     containerClickZone.forEach((e)=>
@@ -21,6 +14,7 @@ window.onload = function()
         e.addEventListener("click", ()=>
         {
             e.querySelector("table").style.display = e.querySelector("table").style.display === "none" ? 'table' : 'none'
+            e.querySelector(".close_button").style.transform = e.querySelector(".close_button").style.transform === "rotate(0deg)" ? "rotate(180deg)" : "rotate(0)"
         })
     })
 }
