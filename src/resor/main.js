7993be017d68bc48c25bed45fa6310a4df6d8887
Rafael <you@example.com>
2021-12-16 21:03:07 -0300

    #7 add arrow icon, missing function to rotate the arrow up when user click to open the card

--- src/resor/main.js
+++ src/resor/main.js
@@ -1,22 +1,27 @@
-window.onload = function(){
+window.onload = function()
+{
 
     const containerClickZone = document.querySelectorAll(".container")
     const table = document.querySelectorAll("table")
     const td = document.querySelectorAll("td")
+    const closeArrowAction = document.querySelectorAll(".close_button")
 
-    td.forEach((e)=>{
+
+    td.forEach((e)=>
+    {
         e.style.display = "block"
     })
 
-    table.forEach((e)=>{
+    table.forEach((e)=>
+    {
         e.style.marginLeft = "2%"
-        e.style.display = "none"
+        e.style.display = "none"   
     })
 
-    containerClickZone.forEach((e)=>{
-        
-        e.addEventListener("click", ()=>{
-            console.log("clicou")
+    containerClickZone.forEach((e)=>
+    {
+        e.addEventListener("click", ()=>
+        {
             e.querySelector("table").style.display = e.querySelector("table").style.display === "none" ? 'table' : 'none'
         })
     })
