e6eb49be6e1f3ce5b0252ab5c60323b3f8136bf5
Rafael Oliveira <rafaelopires@icloud.com>
2022-03-08 21:32:54 -0300

    #1 carosel testing

--- src/resor/js/funtion.js
+++ src/resor/js/funtion.js
@@ -1,15 +1,15 @@
 let item = document.querySelector(".items")
 
 
-setInterval(item.addEventListener('wheel', event => {
+item.addEventListener('wheel', event => {
     if(event.deltaY > 0){
-        console.log('Scroll Up')
         event.target.scrollBy(300, 0)
     }else{
-        console.log('Scroll down')
         event.target.scrollBy(-300, 0)
     }
-}),1800)
+})
+
+
 
 
 
