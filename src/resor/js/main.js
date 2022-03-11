32e41ba23b54c7ed0fde1352f68cce9dbffb007d
Rafael Oliveira <rafaelopires@icloud.com>
2022-03-11 14:49:50 -0300

    #7 arrow bug fixed

--- src/resor/js/main.js
+++ src/resor/js/main.js
@@ -1,5 +1,5 @@
 
-window.addEventListener("scroll", (event)=>{
+window.addEventListener("scroll", ()=>{
     const header = document.querySelector(".header_content")
     header.classList.toggle("sticky", window.scrollY > 90)
 })
